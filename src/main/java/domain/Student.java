ccc93b7ebc7ed2020a8da4b25d48439042398d21
Taras Mazurkevych <mad.taras@gmail.com>
2017-12-17 15:43:40 +0100

    Implement Student class constructor and toJsonObject method

--- src/main/java/domain/Student.java
+++ src/main/java/domain/Student.java
@@ -2,6 +2,7 @@ package domain;
 
 import json.*;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -9,13 +10,33 @@ import java.util.List;
  * Created by Andrii_Rodionov on 1/3/2017.
  */
 public class Student extends BasicStudent {
+    Tuple<String, Integer>[] exams;
 
     public Student(String name, String surname, Integer year, Tuple<String, Integer>... exams) {
-        // ToDo
+        super(name, surname, year);
+        this.exams = exams;
     }
 
     public JsonObject toJsonObject() {
-        // ToDo
-        return null;
+        JsonPair name = new JsonPair("name", new JsonString(this.name));
+        JsonPair surname = new JsonPair("surname", new JsonString(this.surname));
+        JsonPair year = new JsonPair("year", new JsonNumber(this.year));
+
+        List<JsonObject> examsWithMarks = new ArrayList<>();
+
+        for (Tuple<String, Integer> exam : exams) {
+            examsWithMarks.add(new JsonObject(
+                    new JsonPair("course", new JsonString(exam.key)),
+                    new JsonPair("mark", new JsonNumber(exam.value)),
+                    new JsonPair("passed", new JsonBoolean(exam.value >= 3))
+            ));
+        }
+
+        JsonObject[] examsWithMarksArr = new JsonObject[exams.length];
+        examsWithMarks.toArray(examsWithMarksArr);
+
+        JsonPair examsJson = new JsonPair("exams", new JsonArray(examsWithMarksArr));
+
+        return new JsonObject(name, surname, year, examsJson);
     }
 }
