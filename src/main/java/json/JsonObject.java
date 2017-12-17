bfae258b7a382f9043939278814b9d875f8112d0
Taras Mazurkevych <mad.taras@gmail.com>
2017-12-17 11:25:50 +0100

    Implement JsonObject class constructor and toJson() method

--- src/main/java/json/JsonObject.java
+++ src/main/java/json/JsonObject.java
@@ -1,18 +1,37 @@
 package json;
 
+import java.util.ArrayList;
+import java.util.Collections;
+
 /**
  * Created by Andrii_Rodionov on 1/3/2017.
  */
 public class JsonObject extends Json {
+    private ArrayList<JsonPair> collection;
 
     public JsonObject(JsonPair... jsonPairs) {
-        // ToDo
+        collection = new ArrayList<>();
+        Collections.addAll(collection, jsonPairs);
     }
 
     @Override
     public String toJson() {
-        // ToDo
-        return null;
+        StringBuilder result = new StringBuilder("");
+        // Add opening bracket
+        result.append("{");
+
+        for (int i = 0, k = collection.size(); i < k; i++) {
+            result.append(collection.get(i).key);
+            result.append(": ");
+            result.append(collection.get(i).value.toJson());
+
+            if (i != k - 1) result.append(", ");
+        }
+
+        // Add closing bracket
+        result.append("}");
+
+        return result.toString();
     }
 
     public void add(JsonPair jsonPair) {
